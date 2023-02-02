effc4ec9e0f2e8d988551d0c4653877051c38fa8
parkyejin <uiop5809@naver.com>
2023-02-02 22:50:44 +0900

    Feat: Room 나가기, 메시지 실시간

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -16,11 +16,23 @@ function addMessage(message) {
   ul.appendChild(li);
 }
 
+function handleMessageSubmit(event) {
+  event.preventDefault();
+  const input = room.querySelector("input");
+  const value = input.value;
+  socket.emit("new_message", input.value, roomName, () => {
+    addMessage(`You: ${value}`);
+  });
+  input.value = "";
+}
+
 function showRoom() {
   welcome.hidden = true;
   room.hidden = false;
   const h3 = room.querySelector("h3");
   h3.innerText = `Room ${roomName}`;
+  const form = room.querySelector("form");
+  form.addEventListener("submit", handleMessageSubmit);
 }
 
 function handleSubmit(event) {
@@ -37,6 +49,14 @@ form.addEventListener("submit", handleSubmit);
 socket.on("welcome", () => {
   addMessage("someone joined!");
 });
+
+// 유저가 나간다면
+socket.on("bye", () => {
+  addMessage("someone left ㅠㅠ");
+});
+
+// 유저가 메세지 보낸다면
+socket.on("new_message", addMessage);
 
 /* WebSocket */
 // // 프론트에서 socket은 서버 연결을 뜻
